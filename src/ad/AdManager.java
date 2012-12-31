1dfd8900a7ef96797c127dd180725ddf68a3256c
Administrator <Administrator@admin-PC>
2012-12-31 18:12:30 +0800

    Q

--- src/ad/AdManager.java
+++ src/ad/AdManager.java
@@ -1,29 +1,30 @@
 package ad;
 
-import cn.xxd.qr.R;
-
 import android.app.Activity;
 import android.content.Context;
-import android.widget.FrameLayout;
 
 public class AdManager {
 	
 	public static void init(Context ctx){
-		com.tencent.exmobwin.MobWINManager.init(ctx,com.tencent.exmobwin.Type.MOBWIN_BANNER);
+		//net.youmi.android.AdManager.init(ctx,"b91f0970cc72ba26", "a6962e4070cbec1f", 30, false);
+		//com.tencent.exmobwin.MobWINManager.init(ctx,com.tencent.exmobwin.Type.MOBWIN_BANNER);
 	}
 	
 	public static void destroy(){
-		com.tencent.exmobwin.MobWINManager.destroy();
+		//com.tencent.exmobwin.MobWINManager.destroy();
 	}
 	
 	public static void add(Activity act){
-		FrameLayout parent = (FrameLayout)act.findViewById(R.id.ad);
+		/*FrameLayout parent = (FrameLayout)act.findViewById(R.id.ad);
 		if(parent == null){
 			return;
 		}
-		com.tencent.exmobwin.banner.TAdView adView = new com.tencent.exmobwin.banner.TAdView(act); 
+		//
+		//com.tencent.exmobwin.banner.TAdView adView = new com.tencent.exmobwin.banner.TAdView(act); 
+		net.youmi.android.AdView adView = new net.youmi.android.AdView(act); 
+		//
 		FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.FILL_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
-		parent.addView(adView, params);  		
+		parent.addView(adView, params);  	*/	
 	}
 
 }
